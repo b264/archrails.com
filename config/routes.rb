96aa6431d7896173e3eebb8ab38b7238f32b0205
Chris Zempel <zempel@efeqdev.com>
2016-02-14 15:07:07 -0600

    adds contribute page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   resources :suggestions
 
-  get "conduct", to: "home#conduct", as: :conduct
-  get "values",  to: "home#values",  as: :values
-  get "about",   to: "home#about",   as: :about
+  get "conduct",      to: "home#conduct",      as: :conduct
+  get "values",       to: "home#values",       as: :values
+  get "about",        to: "home#about",        as: :about
+  get "contribute",   to: "home#contribute",   as: :contribute
 
   root to: "home#home"
 end
